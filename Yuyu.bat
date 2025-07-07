00d7b488261dd3a06e16e20e754fe513477b211b
stoneboat <asm650c816@gmail.com>
2025-07-07 21:33:03 +0700

    Add files via upload

---
+++ Yuyu.bat
@@ -0,0 +1,9 @@
+copy YuJ.smc Yuyu.smc
+
+
+atlas -d log.txt Yuyu.smc battle_text.txt
+atlas -d log.txt Yuyu.smc battle_name.txt
+atlas -d log.txt Yuyu.smc serifu.txt
+xkas.exe -o Yuyu.smc main.asm
+
+pause
